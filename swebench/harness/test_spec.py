af9a3cec79bf9678e6a165233133ec16d96a86cd
Yihan-Li-cyber <Yihan.Li22@student.xjtlu.edu.cn>
2025-04-04 00:13:10 +0800

    test_spec repo路径补全

--- swebench/harness/test_spec.py
+++ swebench/harness/test_spec.py
@@ -122,7 +122,7 @@ def make_repo_script_list(specs, repo, repo_directory, base_commit, env_name):
     This is the setup script for the instance image.
     """
     setup_commands = [
-        f"git clone -o origin XXXX{repo} {repo_directory}",
+        f"git clone -o origin https://github.com/{repo}.git {repo_directory}",
         f"chmod -R 777 {repo_directory}",  # So nonroot user can run tests
         f"cd {repo_directory}",
         f"git reset --hard {base_commit}",
